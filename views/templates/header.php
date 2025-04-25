e8bcc1020648ffb5b4142c146c61fc974126a1e0
Wilfredery <wilfredery30@gmail.com>
2025-04-25 00:27:25 -0800

    Working with the header

---
+++ views/templates/header.php
@@ -0,0 +1,38 @@
+<header class="header">
+    <div class="header__contenedor">
+        <nav class="header__navegacion">
+            <a href="/registro" class="header__enlace">Registro</a>
+            <a href="/login" class="header__enlace">Iniciar sesion</a>
+        </nav>
+
+        <div class="header__contenido">
+            <a href="">
+                <h1 class="header__logo">
+                    &#60DevWebCamp />
+                </h1>
+            </a>
+        </div>
+
+        <p class="header__texto">Octubre 5-6 - 2023</p>
+        <p class="header__texto header__texto--modalidad">En linea - Presencial</p>
+
+        <a href="/registro" class="header__boton">Comprar pase</a>
+    </div>
+</header>
+
+<div class="barra">
+    <div class="barra__contenido">
+        <a href="" class="barra__logo">
+            <h2 class="barra__logo">
+                &#60DevWebCamp />
+            </h2>
+        </a>
+
+        <nav class="navegacion">
+            <a href="/devwebcamp" class="navegacion__enlace">Evento</a>
+            <a href="/paquetes" class="navegacion__enlace">Paquetes</a>
+            <a href="/workshops-conferencias" class="navegacion__enlace">Workshop/Conferencias</a>
+            <a href="/registro" class="navegacion__enlace">Comprar pase</a>
+        </nav>
+    </div>
+</div>
